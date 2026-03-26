4a4f8ca59991625bc0265492dadd367797a0792d
Ashvini Vibhute <ashvini@joshsoftware.com>
2026-03-26 11:49:13 +0530

    UI changes

--- api/app/controllers/transactions_controller.rb
+++ api/app/controllers/transactions_controller.rb
@@ -1,7 +1,8 @@
 class TransactionsController < ApplicationController
   def index
-    transactions = Transaction.all
-    transactions.with_category(params[:category]) if params[:category]
-    render json: { data: Transaction.all }
+    transactions =  Transaction.all
+    transactions = transactions.with_category(params[:category]) if params[:category]
+    
+    render json: { data: transactions }
   end
 end
